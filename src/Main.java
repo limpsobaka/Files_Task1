ddab34a5816a1a6cd3400c6cfa2a8aa3cc88c7c6
limpsobaka <limpsobaka@yandex.ru>
2022-09-23 18:26:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -31,10 +31,3 @@ public class Main {
     logger.writeLogFile();
   }
 }
-
-
-//        В папке Games создайте несколько директорий: src, res, savegames, temp.+
-//        В каталоге src создайте две директории: main, test.+
-//        В подкаталоге main создайте два файла: Main.java, Utils.java. +
-//        В каталог res создайте три директории: drawables, vectors, icons.+
-//        В директории temp создайте файл temp.txt.
